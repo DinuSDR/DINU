c6a055bc61f1319047d2883a1c41fc920a7e9c59
DineshSDR <dineshraja1109@gmail.com>
2022-03-26 22:19:30 +0530

    ipl project KL

---
+++ src/com/match/MatchFixtures.java
@@ -0,0 +1,17 @@
+package com.match;
+
+public class MatchFixtures {
+
+	private void KingLionRahul() {
+		System.out.println("Lucknow Super Giants");
+	}
+	
+	public static void main(String[] args) {
+		MatchFixtures fix = new MatchFixtures();
+		fix.KingLionRahul();
+		
+		
+	}
+	
+	
+}
